c74b529d599cea0d3e8610dc235d8b479db5bb81
Arunachalam <s.arunachalam.tech@gmail.com>
2025-02-01 13:06:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    API_ID = int(environ.get("API_ID", "22225617"))
+    API_HASH = environ.get("API_HASH", "ef16f7597376f1689663304c954e4493")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7841366478:AAHj71zGlmFbSCspB5TukzLc9nhAbWp0xZo") 
+    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://sarunachalamtech:S.Aruna1155182089@forward.co7br.mongodb.net/?retryWrites=true&w=majority&appName=forward")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "forward")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6072149828"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
